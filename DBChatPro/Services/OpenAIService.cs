6af4c41ac6500f0eb181d53e3d88f96d2f5bfab8
gloryaj <gloryaj99@gmail.com>
2024-09-17 09:25:45 -0600

    Analizar datos columna por columna

--- DBChatPro/Services/OpenAIService.cs
+++ DBChatPro/Services/OpenAIService.cs
@@ -12,10 +12,6 @@ namespace DBChatPro.Services
     {
         public static async Task<AIQuery> GetAISQLQuery(string userPrompt, AIConnection aiConnection)
         {
-            // Configure OpenAI client
-            //string openAIEndpoint = "your-openai-endpoint";
-            //string openAIKey = "your-openai-key";
-            //string openAIDeploymentName = "your-model-deployment-name";
 
             // Crear el cliente de OpenAI con la clave
             var api = new OpenAIClient("your-openai-key");
@@ -38,12 +34,10 @@ namespace DBChatPro.Services
             builder.AppendLine(@"{ ""summary"": ""your-summary"", ""query"":  ""your-query"" }");
             builder.AppendLine("Output ONLY JSON formatted on a single line. Do not use new line characters.");
             builder.AppendLine(@"In the preceding JSON response, substitute ""your-query"" with Microsoft SQL Server Query to retrieve the requested data.");
-            builder.AppendLine(@"In the preceding JSON response, substitute ""your-summary"" with a detailed analysis of the key insights in Spanish. The summary should include specific metrics such as totals, averages, the most frequent or highest values, and any other relevant patterns or trends regardless of the data type. Be specific and provide numerical values where applicable. For example, 'El artículo más vendido fue X con un total de Y ventas, representando Z% del total de ventas.'");
+            builder.AppendLine(@"In the preceding JSON response, substitute ""your-summary"" with an analysis of the query results in Spanish. The summary should include key insights, such as totals, averages, the most frequent or highest values, and any other relevant patterns or trends regardless of the data type.");
             builder.AppendLine("Do not use MySQL or PostgreSQL syntax.");
             //builder.AppendLine("Always include all of the table columns and details.");
             
-            // builder.AppendLine("Always limit the SQL Query to 100 rows.");
-
 
             // Build the AI chat/prompts
             chatHistory.Add(new SystemChatMessage(builder.ToString()));
